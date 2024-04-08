209432c2a6e615683e1fdee950ef67464b3e4f52
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2024-04-08 18:52:38 +0200

    style not found

--- client/src/pages/notFound/NotFound.jsx
+++ client/src/pages/notFound/NotFound.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
 import "./notFound.scss"
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="notFound">
+      <h1>404</h1>
+      <p>Page not found</p>
+      <Link to="/">Go back to the main page</Link>
+    </div>
   )
 }
 
